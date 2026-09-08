f07bd3c1b738da3ef7e0b6257fdf4288b269864b
Moluno-xiii <molunoprogress@gmail.com>
2026-09-08 20:48:06 +0100

    feat: make contact rows navigatable and return to origin after user edits

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -31,7 +31,7 @@ class ContactsController < ApplicationController
 
   def update
     if @contact.update(contact_params)
-      redirect_out_of_frame contacts_path, notice: "Contact updated."
+      redirect_out_of_frame return_path, notice: "Contact updated."
     else
       render :edit, status: :unprocessable_content
     end
@@ -40,6 +40,10 @@ class ContactsController < ApplicationController
   private
     def set_contact
       @contact = current_workspace.contacts.includes(:contact_roles).find(params[:id])
+    end
+
+    def return_path
+      url_from(params[:return_to]) || contacts_path
     end
 
     def contact_params
